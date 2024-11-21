72bfd02ae8a2dbe280b2a06444e6544a2abc2884
Umunna Joseph <umunnajoseph70@gmail.com>
2024-11-21 13:15:05 +0100

    cv update

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import myphoto from './img/myphoto.jpeg';
+import CV from './files/CV.pdf';
 
 const About = () => {
   return (
@@ -17,15 +18,18 @@ const About = () => {
           />
           <div className="text-center md:text-left">
             <p className="text-lg text-gray-700 leading-relaxed">
-              I'm <span className="font-semibold text-blue-600"> Umunna Chimaroke Joseph</span>, a passionate <span className="font-semibold text-blue-600">Frontend Developer</span> and <span className="font-semibold text-blue-600">Technical Writer</span>.
-              I have a <span className="font-semibold">National Diploma in Computer Science</span> and a <span className="font-semibold">Bachelor's degree in Statistics</span>, giving me a balanced foundation in both technical and analytical skills.
+              I'm <span className="font-semibold text-blue-600">Umunna Chimaroke Joseph</span>, a passionate{' '}
+              <span className="font-semibold text-blue-600">Frontend Developer</span> and{' '}
+              <span className="font-semibold text-blue-600">Technical Writer</span>.
+              I have a <span className="font-semibold">Bachelor's degree in Statistics</span> and a{' '}
+              <span className="font-semibold">National Diploma in Computer Science</span>, giving me a balanced foundation in both technical and analytical skills.
             </p>
             <p className="mt-4 text-lg text-gray-700 leading-relaxed">
               After completing an internship with <span className="font-semibold text-blue-600">AltSchool Africa</span>, I now work as a freelancer, creating impactful projects and writing technical articles. I’m dedicated to building user-centered solutions and expanding my skill set.
             </p>
             <div className="mt-6 flex justify-center md:justify-start space-x-4">
               <a
-                href="https://drive.google.com/file/d/1lmj8qqV-1HAxiqDMSBO7jal8GCV98I_W/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1iAr6y5g4IDS24fbofgC6HFLdcSJp2R9k/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block bg-blue-500 text-white py-2 px-6 rounded-full font-medium hover:bg-blue-600 transition duration-300"
@@ -33,7 +37,7 @@ const About = () => {
                 View Resume
               </a>
               <a
-                href="https://drive.google.com/uc?export=download&id=1lmj8qqV-1HAxiqDMSBO7jal8GCV98I_W"
+                href={CV}
                 download
                 className="inline-block bg-gray-300 text-gray-800 py-2 px-6 rounded-full font-medium hover:bg-gray-400 transition duration-300"
               >
@@ -64,14 +68,12 @@ const About = () => {
               <li>
                 <span className="font-semibold">KSolutionsNG Consulting</span> – Frontend Engineer, April 2023 – Present.
               </li>
-              
               <li>
                 <span className="font-semibold">Freelance Developer</span> – Currently working on freelance projects and writing articles on the side.
               </li>
               <li>
-                <span className="font-semibold">Royalsmith Trucking LLC</span> – Frontend Engineer, May. 2022 – July 2022.
+                <span className="font-semibold">Royalsmith Trucking LLC</span> – Frontend Engineer, Jan. 2022 – March 2023.
               </li>
-              
             </ul>
           </div>
 
@@ -79,12 +81,22 @@ const About = () => {
           <div className="p-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition duration-300 border-r-4 border-blue-600">
             <h3 className="text-2xl font-semibold text-gray-800 mb-4">Hobbies & Interests</h3>
             <ul className="list-disc list-inside text-gray-600">
-              <li>Playing sports like <span className="font-semibold text-blue-600">football</span>, <span className="font-semibold text-blue-600">basketball</span>, and <span className="font-semibold text-blue-600">volleyball</span>.</li>
+              <li>
+                Playing sports like <span className="font-semibold text-blue-600">football</span>,{' '}
+                <span className="font-semibold text-blue-600">basketball</span>, and{' '}
+                <span className="font-semibold text-blue-600">volleyball</span>.
+              </li>
               <li>Exploring new technologies and creating interactive user experiences.</li>
               <li>Traveling to new places and learning about diverse cultures.</li>
               <li>Playing <span className="font-semibold text-blue-600">video games</span> to unwind and relax.</li>
-              <li>Reading books, especially on <span className="font-semibold text-blue-600">self-development</span> and <span className="font-semibold text-blue-600">technology trends</span>.</li>
-              <li>Listening to podcasts on <span className="font-semibold text-blue-600">tech innovations</span> and <span className="font-semibold text-blue-600">entrepreneurship</span>.</li>
+              <li>
+                Reading books, especially on <span className="font-semibold text-blue-600">self-development</span> and{' '}
+                <span className="font-semibold text-blue-600">technology trends</span>.
+              </li>
+              <li>
+                Listening to podcasts on <span className="font-semibold text-blue-600">tech innovations</span> and{' '}
+                <span className="font-semibold text-blue-600">entrepreneurship</span>.
+              </li>
               <li>Volunteering and mentoring new tech enthusiasts in the community.</li>
             </ul>
           </div>
